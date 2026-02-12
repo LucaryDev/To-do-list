184eb42bb12681ec4fe2ee3b6440082f6ceddea4
LucaryDev <242092893+LucaryDev@users.noreply.github.com>
2026-02-12 19:24:08 -0300

    feat(js): adiciona captura de elementos da lista de tarefas

---
+++ script.js
@@ -0,0 +1,11 @@
+//HTML
+// input
+let input = document.getElementById("nome")
+ 
+//botao
+const botaoAdicionarTarefa = document.getElementById("botaoNome")
+
+// ID
+const ulPendente = document.getElementById("pendente")
+const ulConcluido = document.getElementById("concluido")
+
